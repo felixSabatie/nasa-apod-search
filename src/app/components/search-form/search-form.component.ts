ab9cd2a11b398c6c17507e86630fd92c293253c0
Félix Sabatié <felix.sabatie@gmail.com>
2019-04-27 12:14:59 -0400

    sass to scss

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.sass']
+  styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent implements OnInit {
 
