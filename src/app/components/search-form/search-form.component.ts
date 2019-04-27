057d27cd97bae5631754d3055a8ea43469f468b3
Félix Sabatié <felix.sabatie@gmail.com>
2019-04-27 15:01:58 -0400

    Send click event to home

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
@@ -6,12 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent implements OnInit {
+  @Output() onSearch = new EventEmitter();
+
   startDate = new Date();
   endDate = new Date();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  search() {
+    this.onSearch.emit({startDate: this.startDate, endDate: this.endDate});
   }
 
 }
