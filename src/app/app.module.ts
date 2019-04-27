522f9c4d51007cffc4ed231f4cbc55e44e264f1c
Félix Sabatié <felix.sabatie@gmail.com>
2019-04-27 12:10:35 -0400

    Generate form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
