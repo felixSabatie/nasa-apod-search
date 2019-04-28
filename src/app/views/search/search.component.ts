e36a138845f143ab54e19015c3d5659610669bce
Félix Sabatié <felix.sabatie@gmail.com>
2019-04-28 10:50:17 -0400

    Load more method implemented

--- src/app/views/search/search.component.ts
+++ src/app/views/search/search.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NasaApiService } from 'src/app/services/nasa-api.service';
 import { ApodInfos } from 'src/app/models/ApodInfos.model';
 import { DateService } from 'src/app/utils/date.service';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -12,7 +13,9 @@ import { DateService } from 'src/app/utils/date.service';
 export class SearchComponent implements OnInit {
   startDate: Date;
   endDate: Date;
+  lastDate: Date;
   apods: ApodInfos[];
+
   fetching = false;
   error = false;
 
@@ -27,17 +30,27 @@ export class SearchComponent implements OnInit {
       if (isNaN(this.startDate.getTime()) || isNaN(this.endDate.getTime()) || this.startDate > this.endDate) {
         this.error = true;
       } else {
-        this.searchApi();
+        if (this.dateService.differenceInDays(this.startDate, this.endDate) > 10) {
+          this.lastDate = this.dateService.addDays(this.startDate, 10);
+        } else {
+          this.lastDate = this.endDate;
+        }
+
+        this.searchApi(this.startDate, this.lastDate).subscribe(apods => {
+          this.apods = apods;
+        });
       }
     });
   }
 
-  searchApi() {
-    // TODO limit number of images requested
-    this.fetching = true;
-    this.nasaApiService.search(this.startDate, this.endDate).subscribe(apods => {
-      this.apods = apods;
-      this.fetching = false;
+  searchApi(startDate: Date, endDate: Date): Observable<ApodInfos[]> {
+    return new Observable<ApodInfos[]>(subscriber => {
+      this.fetching = true;
+
+      this.nasaApiService.search(startDate, endDate).subscribe(apods => {
+        this.fetching = false;
+        subscriber.next(apods);
+      });
     });
   }
 
@@ -46,6 +59,20 @@ export class SearchComponent implements OnInit {
       startDate: this.dateService.dateToUrlParam(e.startDate),
       endDate: this.dateService.dateToUrlParam(e.endDate),
     } });
+  }
+
+  loadMore() {
+    if (this.lastDate < this.endDate) {
+      const startDate = this.lastDate;
+      this.lastDate = this.dateService.addDays(this.lastDate, 10);
+      if (this.lastDate > this.endDate) {
+        this.lastDate = this.endDate;
+      }
+
+      this.searchApi(startDate, this.lastDate).subscribe(apods => {
+        this.apods = [...this.apods, ...apods];
+      });
+    }
   }
 
 }
